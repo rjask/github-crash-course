81242d447898e2f32c50b6615ff053fc5c9cef3c
GULSHAN RAJ <gulshanvlog@gmail.com>
2024-02-03 01:47:08 +0530

    added isAbove18 function

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const age = 24;
 const name = "gulshan raj"
 const dob = "12/05/1998"
 function sayName(){
-    console.log("my name is " + name)
+    console.log("my name is " + name1)
+}
+function isAbove18(){
+    return age>=18
 }
