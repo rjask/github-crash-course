814a6b1f03ddd1bcd367374f0bc9386c2d916552
GULSHAN RAJ <gulshanvlog@gmail.com>
2024-02-03 10:15:14 +0530

    new feature added

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const dob = "12/05/1998"
 function sayName(){
     console.log("my name is " + name1)
 }
+function isAbove18(){
+    return age>=18
+}
+function newFeatureAdd(){
+    console.log("new feature added")
+}
